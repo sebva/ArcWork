6c1b04f074081271382f4438b2c4f5aa6f7e25a3
rails <rails@rails-vbox.(none)>
2014-02-21 14:29:46 +0100

    ajout du projet squelette

---
+++ ArcWork/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArcWork::Application.config.secret_key_base = '275059c52ab898d411564168c83061a11f4b2ccaee0c810dd4bb454b18db2fca69df62a1519b00ec3124c958ddf5470e3abe2b0c4639d368af44bdc19bfb4027'
