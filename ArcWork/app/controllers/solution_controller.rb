605d69ac6184744ad904f5451dc19964c801756d
Sébastien Vaucher <seb.vaucher@gmail.com>
2014-02-28 15:54:12 +0100

    Generate controllers

---
+++ ArcWork/app/controllers/solution_controller.rb
@@ -0,0 +1,10 @@
+class SolutionController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def comment
+  end
+end
---
+++ ArcWork/test/controllers/solution_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class SolutionControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get create" do
+    get :create
+    assert_response :success
+  end
+
+  test "should get comment" do
+    get :comment
+    assert_response :success
+  end
+
+end
