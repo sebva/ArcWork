3acc93ff9936f6eb1716ed4bed8cdfb59556abd1
Sébastien Vaucher <seb.vaucher@gmail.com>
2014-03-18 17:04:52 +0100

    UsersController (first version)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,47 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def create
+    @user = User.new(get_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render 'new'
+    end
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(get_params)
+      redirect_to users_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def get_params
+    params[:user].permit(:name, :email, :rank)
   end
 end
