3a3e8f7e69fa1435550ab7a19f461f9519f735d8
adrian-moll <moll.adrian@gmail.com>
2014-03-25 17:14:06 +0100

    Personalize message

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,98 @@
 class CoursesController < ApplicationController
   def index
+    if(!current_user.nil? && current_user.isDean?)
+      couses_add_view_init
+
+    else
+      redirect_to root_url
+    end
+
+  end
+
+  def show
+    redirect_to root_url
+  end
+
+  def edit
+    if(!current_user.nil? && current_user.isDean?)
+      @course = Course.find(params[:id])
+      @professors = User.all
+    else
+      redirect_to root_url
+    end
+  end
+
+  def new
+    if(!current_user.nil? && current_user.isDean?)
+      @course = Course.new
+      @professors = User.all
+      @course.start_year = get_current_year
+    else
+      redirect_to root_url
+    end
+  end
+
+  def create
+    if(!current_user.nil? && current_user.isDean?)
+      @course = Course.create(params[:course].permit(:name, :start_year, :professor_id))
+      if @course.valid?
+        redirect_to action: "index"
+
+      else
+        @course.errors.each do |attribute, error|
+          flash.now[:error] = attribute.to_s + " " +  error.to_s
+        end
+        @course.errors.clear
+        @professors = User.all
+        render "new"
+      end
+    else
+      redirect_to root_url
+    end
+  end
+
+  def update
+    if(!current_user.nil? && current_user.isDean?)
+      @course = Course.find(params[:id])
+
+      if @course.update(params[:course].permit(:name, :start_year, :professor_id))
+        redirect_to action: "index"
+      else
+        @course.errors.each do |attribute, error|
+        flash.now[:error] = attribute.to_s + " " +  error.to_s
+        end
+        @course.errors.clear
+        @professors = User.all
+        render 'edit'
+      end
+    else
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+    if(!current_user.nil? && current_user.isDean?)
+      @course = Course.find(params[:id])
+      if @course.destroy
+        redirect_to action: "index"
+      else
+        render 'delete'
+      end
+    else
+      redirect_to root_url
+    end
+
+  end
+
+  def get_current_year()
+    year = Time.now.year;
+    if Time.now.month < 8
+      year -= 1
+    end
+    return year
+  end
+
+  def couses_add_view_init()
     @curr_year = params[:year]
     if params[:year].nil?
       @curr_year = get_current_year
@@ -26,66 +119,4 @@ class CoursesController < ApplicationController
     @courses = Course.where(start_year: @curr_year)
   end
 
-  def show
-    @course = Course.find(params[:id])
-  end
-
-  def edit
-    @course = Course.find(params[:id])
-    @professors = User.all
-  end
-
-  def new
-    @course = Course.new
-    @professors = User.all
-    @course.start_year = get_current_year
-  end
-
-  def create
-    @course = Course.create(params[:course].permit(:name, :start_year, :professor_id))
-    if @course.valid?
-      redirect_to action: "index"
-
-    else
-      @course.errors.each do |attribute, error|
-        flash.now[:error] = attribute.to_s + " " +  error.to_s
-      end
-      @course.errors.clear
-      @professors = User.all
-      render "new"
-    end
-  end
-
-  def update
-    @course = Course.find(params[:id])
-
-    if @course.update(params[:course].permit(:name, :start_year, :professor_id))
-      redirect_to action: "index"
-    else
-      @course.errors.each do |attribute, error|
-      flash.now[:error] = attribute.to_s + " " +  error.to_s
-      end
-      @course.errors.clear
-      @professors = User.all
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @course = Course.find(params[:id])
-    if @course.destroy
-      redirect_to action: "index"
-    else
-      render 'delete'
-    end
-
-  end
-
-  def get_current_year()
-    year = Time.now.year;
-    if Time.now.month < 8
-      year -= 1
-    end
-    return year
-  end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, :presence=> true
+  validates :start_year, :inclusion => 2000..2200
   belongs_to :professor, :class_name => 'User'
   validates_associated :professor
   has_many :homeworks
