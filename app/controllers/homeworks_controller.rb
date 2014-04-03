15d5e583c63797f0bd74e17158c2f05827306bd3
Sébastien Vaucher <seb.vaucher@gmail.com>
2014-04-03 23:06:13 +0200

    Homework controller ready for production use

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -3,8 +3,12 @@ class HomeworksController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @course = Course.find(params[:course_id])
-    @homework = @course.homeworks
+    if current_user.isProfessor? or current_user.isStudent?
+      @course = Course.find(params[:course_id])
+      @homework = @course.homeworks
+    else
+      redirect_to home_index_path
+    end
   end
 
   def show
@@ -15,52 +19,73 @@ class HomeworksController < ApplicationController
       users = @homework.solutions.select(:user_id).distinct
       @solutions = []
       for user in users do
-        last_solution = Solution.where("user_id = ?", user.user_id).sort_by{ |sol| sol.date }.reverse.first
+        last_solution = Solution.where("user_id = ?", user.user_id).sort_by { |sol| sol.date }.reverse.first
         @solutions.append(last_solution)
       end
 
       render '_show_uniq_solutions'
-    else
+    elsif current_user.isStudent?
       render '_show_homework'
+    else
+      redirect_to home_index_path
     end
   end
 
   def new
+
     @course = Course.find(params[:course_id])
     @homework = Homework.new
+    if not current_user.isProfessor?
+      redirect_to course_homeworks_path(@course)
+    end
   end
 
   def edit
     @course = Course.find(params[:course_id])
     @homework = Homework.find(params[:id])
+    if not current_user.isProfessor?
+      redirect_to course_homeworks_path(@course)
+    end
   end
 
   def create
-    @course = Course.find(params[:course_id])
-    @homework = Homework.new(get_params)
-    @homework.course = @course
-    if @homework.save
-      redirect_to [@course, @homework]
+    if current_user.isProfessor?
+      @course = Course.find(params[:course_id])
+      @homework = Homework.new(get_params)
+      @homework.course = @course
+      if @homework.save
+        redirect_to [@course, @homework]
+      else
+        render 'new'
+      end
     else
-      render 'new'
+      redirect_to course_homeworks_path(@course)
     end
   end
 
   def update
-    @course = Course.find(params[:course_id])
-    @homework = Homework.find(params[:id])
-    if @homework.update(get_params)
-      redirect_to [@course, @homework]
+    if current_user.isProfessor?
+      @course = Course.find(params[:course_id])
+      @homework = Homework.find(params[:id])
+      if @homework.update(get_params)
+        redirect_to [@course, @homework]
+      else
+        render 'edit'
+      end
     else
-      render 'edit'
+      redirect_to course_homeworks_path(@course)
     end
   end
 
   def destroy
-    @homework = Homework.find(params[:id])
-    @homework.destroy
-    course = Course.find(params[:course_id])
-    redirect_to course_homeworks_url(course)
+    if current_user.isProfessor?
+      @homework = Homework.find(params[:id])
+      @homework.destroy
+      course = Course.find(params[:course_id])
+      redirect_to course_homeworks_url(course)
+    else
+      redirect_to course_homeworks_path(@course)
+    end
   end
 
   private
