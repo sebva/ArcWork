364543c458e524ba098786482494c848c8f565e5
Sébastien Vaucher <seb.vaucher@gmail.com>
2014-04-11 18:24:58 +0200

    Remove "Forgot your password"

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
 
   validates :password,              :presence => true,    :if => :password_required?
   validates :password_confirmation, :presence => true,    :if => :password_required?
